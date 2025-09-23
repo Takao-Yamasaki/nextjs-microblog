d0cc69f557d17da070034552a64d38b499802c09
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-09-23 20:39:48 +0900

    add:Linkコンポーネント

--- nextjs-microblog/pages/posts/firstPost.js
+++ nextjs-microblog/pages/posts/firstPost.js
@@ -1,10 +1,13 @@
+import Link from "next/link";
+
 // nfと入力すると雛形が記述される
 // /posts/firstPostでルーティングされる
+// aタグだとリロードが走る
 export default function FirstPost() {
   return (
     <div>
       <h1>最初の投稿</h1>
-      <h2>ホームへ戻る</h2>
+      <Link href="/">ホームへ戻る</Link>
     </div>
   );
 }
