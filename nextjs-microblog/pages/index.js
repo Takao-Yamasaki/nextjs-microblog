2d50bbe4bf980c4ebff749ab0154e65bf4e94616
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-09-25 22:45:01 +0900

    add:トップページのスタイル

--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -5,6 +5,8 @@ import styles from "@/styles/Home.module.css";
 import Link from "next/link";
 import Layout from "@/components/Layout";
 
+import utilStyle from "../styles/utils.module.css"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -17,6 +19,56 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   return <Layout>
-    hogehoge
+    <section className={utilStyle.headingMd}>
+      <p>
+        私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavaScriptです
+      </p>
+    </section>
+
+    <section>
+      <h2>📝エンジニアのブログ</h2>
+      <div className={styles.grid}>
+        <article>
+          <Link href="/">
+            <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+          </Link>
+          <Link href="/" className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </Link>
+          <br/>
+          <small className={utilStyle.lightText}>February 23, 2023</small>
+        </article>
+        <article>
+          <Link href="/">
+            <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+          </Link>
+          <Link href="/" className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </Link>
+          <br/>
+          <small className={utilStyle.lightText}>February 23, 2023</small>
+        </article>
+        <article>
+          <Link href="/">
+            <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+          </Link>
+          <Link href="/" className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </Link>
+          <br/>
+          <small className={utilStyle.lightText}>February 23, 2023</small>
+        </article>
+        <article>
+          <Link href="/">
+            <img src="/images/thumbnail01.jpg" className={styles.thumbnailImage} />
+          </Link>
+          <Link href="/" className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </Link>
+          <br/>
+          <small className={utilStyle.lightText}>February 23, 2023</small>
+        </article>
+      </div>
+    </section>
   </Layout>;
 }
