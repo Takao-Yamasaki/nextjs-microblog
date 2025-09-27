76337cda241026a4d5a6780647a6b24f82d3986f
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2025-09-28 06:19:35 +0900

    add:ファイル名のオブジェクトを返す関数

--- nextjs-microblog/lib/post.js
+++ nextjs-microblog/lib/post.js
@@ -28,3 +28,15 @@ export function getPostData() {
   });
   return allPostsData;
 }
+
+// getStaticPathでreturnで使うpathを取得する
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postDirectory);
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    }
+  })
+}
